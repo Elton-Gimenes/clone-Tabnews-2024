db225f5b1ff6a1851e3222cf8ac1721d3651c06e
Elton-Gimenes <131602904+Elton-Gimenes@users.noreply.github.com>
2025-09-06 02:37:17 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-Tabnews-2024
+
 Versão clone do software Tabnews, implementação de projeto + escalabilidade com estabilidade.
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-function Home(){
-    return <h1>usar drogas só é ilegal se vc for preso :)</h1>
+function Home() {
+  return <h1>usar drogas só é ilegal se vc for preso :)</h1>;
 }
 
 export default Home;
