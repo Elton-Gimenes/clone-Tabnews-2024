fbfde901b1240ce930045f99d65240ac6d90d823
Elton-Gimenes <131602904+Elton-Gimenes@users.noreply.github.com>
2025-09-30 16:39:48 +0000

    atualiza a frase inicial

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,11 @@
 function Home() {
-  return <h1>usar drogas só é ilegal se vc for preso :)</h1>;
+  return (
+    <h1>
+      {" "}
+      O mundo é de quem faz. Mantenha o foco no plano e celebre cada pequeno
+      avanço. O resultado virá com a persistência. :)
+    </h1>
+  );
 }
 
 export default Home;
